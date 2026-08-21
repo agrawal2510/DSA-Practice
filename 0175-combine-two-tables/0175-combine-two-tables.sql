9e002b29485903a6a72690882daa39780852ebe6
agrawal2510 <dikshaagrawal149@gmail.com>
2026-08-21 16:30:12 +0530

    Time: 453 ms (48.62%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,4 +1,4 @@
 # Write your MySQL query statement below
-SELECT firstName , lastName , city , state FROM Person
-LEFT JOIN Address 
+SELECT firstName , lastName , city , state
+FROM Address Right JOIN Person 
 ON Person.PersonId = Address.PersonId;
